6fa8059bd0ffe2ead2e2ebd327133341d4299762
ilzamafif <ilzamafif27@gmail.com>
2021-06-30 13:56:41 +0700

    menampilkan kategori

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category; //LOAD MODEL CATEGORY
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        //BUAT QUERY KE DATABASE MENGGUNAKAN MODEL CATEGORY DENGAN MENGURUTKAN BERDASARKAN CREATED_AT DAN DISET DESCENDING, KEMUDIAN PAGINATE(10) BERARTI HANYA ME-LOAD 10 DATA PER PAGENYA
+        //YANG MENARIK ADALAH FUNGSI WITH(), DIMANA FUNGSI INI DISEBUT EAGER LOADING
+        //ADAPUN NAMA YANG DISEBUTKAN DIDALAMNYA ADALAH NAMA METHOD YANG DIDEFINISIKAN DIDALAM MODEL CATEGORY
+        //METHOD TERSEBUT BERISI FUNGSI RELATIONSHIPS ANTAR TABLE
+        //JIKA LEBIH DARI 1 MAKA DAPAT DIPISAHKAN DENGAN KOMA, 
+        // CONTOH: with(['parent', 'contoh1', 'contoh2'])
+        $category = Category::with(['parent'])->orderBy('created_at', 'DESC')->paginate(10);
+      
+        //QUERY INI MENGAMBIL SEMUA LIST CATEGORY DARI TABLE CATEGORIES, PERHATIKAN AKHIRANNYA ADALAH GET() TANPA ADA LIMIT
+        //LALU getParent() DARI MANA? METHOD TERSEBUT ADALAH SEBUAH LOCAL SCOPE
+        $parent = Category::getParent()->orderBy('name', 'ASC')->get();
+      
+        //LOAD VIEW DARI FOLDER CATEGORIES, DAN DIDALAMNYA ADA FILE INDEX.BLADE.PHP
+        //KEMUDIAN PASSING DATA DARI VARIABLE $category & $parent KE VIEW AGAR DAPAT DIGUNAKAN PADA VIEW TERKAIT
+        return view('categories.index', compact('category', 'parent'));
+    }
+  
+    //METHOD LAINNYA DISINI JIKA ADA
+}
