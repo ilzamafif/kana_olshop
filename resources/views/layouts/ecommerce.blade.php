faa0fa4d5e8e53e73e37c93f0259374d576f092f
ilzamafif <ilzamafif27@gmail.com>
2021-07-01 09:52:55 +0700

    menampilkan product dan ubah templating

--- resources/views/layouts/ecommerce.blade.php
+++ resources/views/layouts/ecommerce.blade.php
@@ -43,7 +43,8 @@
         <div class="container-fluid">
           <!-- Brand and toggle get grouped for better mobile display -->
           <a class="navbar-brand logo_h" href="{{ url('/') }}">
-            <img src="https://daengweb.id/front/dw-theme/images/logo-head.png" alt="">
+            <!-- <img src="https://daengweb.id/front/dw-theme/images/logo-head.png" alt=""> -->
+            <h4>Kana Olshop</h4>
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="icon-bar"></span>
